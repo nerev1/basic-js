8bd66a955e392ff3f7aaf5cf6a960f9a88acb624
Andrei Harbuz <nerev94@gmail.com>
2020-03-22 23:48:10 +0300

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,48 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result = '';
+    let separator;
+    let additionSeparator;
+    let repeatTimes;
+    let additionRepeatTimes;
+
+    if (typeof options['repeatTimes'] == 'number') {
+        repeatTimes = options.repeatTimes;
+    } else {
+        repeatTimes = 1;
+    }
+
+    if (typeof options['additionRepeatTimes'] == 'number') {
+        additionRepeatTimes = options.additionRepeatTimes;
+    } else if ('addition' in options) {
+        additionRepeatTimes = 1;
+    } else {
+        additionRepeatTimes = 0;
+    }
+
+    if (('separator' in options)) {
+        separator = options.separator;
+    } else {
+        separator = '+';
+    }
+    
+    if (('additionSeparator' in options)) {
+        additionSeparator = options.additionSeparator;
+    } else {
+        additionSeparator = '+';
+    }
+
+    for (let i = 0; i < repeatTimes; i++) {
+        result += str;
+        for (let j = 0; j < additionRepeatTimes; j++) {
+            result += options.addition;
+            if (j < additionRepeatTimes - 1) {
+                result += additionSeparator;
+            }
+        }
+        if (i < repeatTimes - 1) {
+            result += separator;
+        }
+    }
+    return result;
 };
   
