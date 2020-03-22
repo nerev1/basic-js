7c6bfdc6f1e58e022851812bb8e197e9ba607880
Andrei Harbuz <nerev94@gmail.com>
2020-03-22 20:56:30 +0300

    feat: solve chain make

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,37 @@
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!arguments.length) this.chain.push('');
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if ((typeof position != 'number') || 
+    (position - Math.floor(position) != 0) || 
+    (position < 1) ||
+    (position > this.getLength())) {
+      this.chain = [];
+      throw new Error;
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = '';
+    this.chain.forEach( (item, index) => {
+      result += '( ' + item + ' )';
+      result += (index == this.chain.length - 1) ? '' : '~~';
+    });
+    this.chain = [];
+    return result;
   }
 };
 
